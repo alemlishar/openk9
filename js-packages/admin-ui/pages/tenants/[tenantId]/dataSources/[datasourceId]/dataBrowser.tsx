ce5776adfe919221db83c66b1fa1e41caf57d8a7
alemlishar <alextopten03@gmail.com>
2021-06-28 05:33:58 +0200

    calling the real api and presenting as list, left to integrate with the expandable demo

--- js-packages/admin-ui/pages/tenants/[tenantId]/dataSources/[datasourceId]/dataBrowser.tsx
+++ js-packages/admin-ui/pages/tenants/[tenantId]/dataSources/[datasourceId]/dataBrowser.tsx
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { createUseStyles } from "react-jss";
 import { useRouter } from "next/router";
 import useSWR from "swr";
@@ -23,7 +23,11 @@ import { format } from "date-fns";
 import ClayForm, { ClayInput } from "@clayui/form";
 import ClayList from "@clayui/list";
 
-import { firstOrString, ThemeType } from "@openk9/search-ui-components";
+import {
+  firstOrString,
+  SearchResultsList,
+  ThemeType,
+} from "@openk9/search-ui-components";
 import {
   doSearch,
   doSearchDatasource,
@@ -35,6 +39,7 @@ import { DataSourceNavBar } from "../../../../../components/DataSourceNavBar";
 import { Layout } from "../../../../../components/Layout";
 import { useLoginCheck, useLoginInfo } from "../../../../../state";
 import { red } from "ansicolor";
+//const searchResults = [];
 const dataListNew = [
   {
     name: "a1",
@@ -117,28 +122,30 @@ function Inner({
 
   const loginInfo = useLoginInfo();
 
-  const { data: finalData } = useSWR(`/api/v2/datasource/${datasourceId}`, () =>
-    getDataSourceInfo(datasourceId, loginInfo),
+  const { data: datasource } = useSWR(
+    `/api/v2/datasource/${datasourceId}`,
+    () => getDataSourceInfo(datasourceId, loginInfo),
   );
   //finalData === datasource;
 
-  console.log("getted result of Datasource finalData:" + finalData?.jsonConfig);
 
-  const { data: searchResults } = useSWR(``, () =>
-    doSearchDatasource(
-      {
-        searchQuery: [{ tokenType: "TEXT", values: [""] }],
-        range: [0, 20],
-      },
-      datasourceId,
-      loginInfo,
-    ),
+  const { data: searchResults } = useSWR(
+    `/api/searcher/v1/search/${datasourceId}`,
+    () =>
+      doSearchDatasource(
+        {
+          searchQuery: [],
+          range: [0, 20],
+        },
+        datasourceId,
+        loginInfo,
+      ),
   );
+  console.log(searchResults);
 
   if (!searchResults) {
     return <span className="loading-animation" />;
-  }
-
+  } 
   return (
     <>
       <div className={classes.settingHeader}>
@@ -150,33 +157,52 @@ function Inner({
       {!searchResults ? (
         <span className="loading-animation" />
       ) : (
-        searchResults.result.map((res) => (
+
+        <span className="loading-animation" />
+        /*searchResults.result.map((res) => (
+
+
+
           <ResultRenderer key={res.source.id} res={res} />
-        ))
+
+
+          
+        ))*/
       )}
+
+ 
+      <CreateDainamicListElement searchResults ={searchResults}/>
     </>
   );
+  //export default searchResults;
 }
 
-function Draw({ list, record, setRecord }) {
-  function toggle({ name, description }) {
-    let temp = record.name;
-    if (temp === name) {
-      setRecord({ name: "", description: "" });
+
+
+
+function Draw({ list, record, setRecord, id }) {
+  console.log("list" + list.source);
+  console.log("record" + record.id);
+
+  function toggle({ id, rawContent }) {
+    alert(id + rawContent);
+    let temp = record.id;
+    if (temp === id) {
+      setRecord({ id: "", rawContent: "" });
     } else {
-      temp == name;
-      setRecord({ name, description });
+      temp == id;
+      console.log("id :" + id + "rawcontent :" + rawContent);
+      setRecord({ id, rawContent });
     }
 
-    console.log(record.name + description);
   }
 
   return (
     <div>
       <ClayList.Item flex value={list} onClick={() => toggle(list)}>
-        <ClayList.ItemField> {list.name} </ClayList.ItemField>
+        <ClayList.ItemField> {list.source.rawContent} </ClayList.ItemField>
       </ClayList.Item>
-      {list.name == record.name && (
+      {list.source.contentId == record.id && (
         <div style={{ height: "300px", border: "2px solid gray" }}>
           <ClayInput
             id={list.name}
@@ -184,24 +210,30 @@ function Draw({ list, record, setRecord }) {
             type="text"
             disabled={true}
           ></ClayInput>
-          <span>{list.description}</span>
+          <span>{list.source.rawContent }</span>
         </div>
       )}
     </div>
   );
 }
 
+//);
+
 function DSDataBrowser() {
+
+  
+
   const classes = useStyles();
-  const [dataList, setDatalist] = useState(dataListNew);
-  const [record, setRecord] = useState({ name: "", description: "" });
-
-  const [showDescriptionIdentifier, setshowSelectedIdentifier] = useState(null);
-
+  const [result, setResult] = useState(null);
   const { query } = useRouter();
-  const tenantId = query.tenantId && firstOrString(query.tenantId);
+  
   const datasourceId = query.datasourceId && firstOrString(query.datasourceId);
   const dataSourceInt = parseInt(datasourceId || "NaN");
+  const [identifier, setIdentifier] = useState(dataSourceInt);
+  
+ 
+  
+  const tenantId = query.tenantId && firstOrString(query.tenantId);
 
   const { loginValid } = useLoginCheck();
   if (!loginValid) return <span className="loading-animation" />;
@@ -210,6 +242,7 @@ function DSDataBrowser() {
     return null;
   }
 
+  
   return (
     <>
       <Layout
@@ -232,32 +265,7 @@ function DSDataBrowser() {
       >
         <div className={classes.root}>
           <Inner tenantId={parseInt(tenantId)} datasourceId={dataSourceInt} />
-          {
-            <ClayForm.Group>
-              <label htmlFor="basicInputText">Search Input</label>
-              <ClayInput
-                id="basicInputText"
-                placeholder="Insert your keyword"
-                type="text"
-              />
-            </ClayForm.Group>
-          }
-          {
-            <ClayList>
-              <ClayList.Header>
-                <span> List Of Indexed Document Total N0: 12000 </span>
-              </ClayList.Header>
-
-              {dataList.map((list, index) => (
-                <Draw
-                  key={index}
-                  list={list}
-                  record={record}
-                  setRecord={setRecord}
-                />
-              ))}
-            </ClayList>
-          }
+          
         </div>
         <div></div>
       </Layout>
@@ -266,3 +274,45 @@ function DSDataBrowser() {
 }
 
 export default DSDataBrowser;
+
+
+function CreateDainamicListElement ({searchResults}){
+
+  const [dataList, setDatalist] = useState([]);
+  const [record, setRecord] = useState({ id:"", rawContent: ""});
+//console.log(">>>>> " +  JSON.stringify(searchResults))
+
+useEffect(()=> {
+setDatalist(searchResults.result)
+},[searchResults])
+  return(
+    <>
+    <ClayForm.Group>
+      <label htmlFor="basicInputText">Search Input</label>
+      <ClayInput
+        id="basicInputText"
+        placeholder="Insert your keyword"
+        type="text"
+      />
+    </ClayForm.Group>
+  
+  
+    <ClayList>
+      <ClayList.Header>
+        <span> List Of Indexed Document Total N0: 12000 </span>
+      </ClayList.Header>
+
+      {dataList && dataList?.map((list, index) => (
+        <Draw
+          key={index}
+          list={list}
+          record={record}
+          setRecord={setRecord}
+          id = {list.source.contentId}
+        />
+      ))}
+    </ClayList>
+    </>
+  )
+
+}
